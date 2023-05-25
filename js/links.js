c7a0838fe73ea85a7aa62b332110c36f6ce65955
Akhil Mahesh <124284817+Akhil-Mahesh@users.noreply.github.com>
2023-05-25 11:57:59 +0530

    Add files via upload

---
+++ js/links.js
@@ -0,0 +1,52 @@
+export const links = [
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/akhil--mahesh",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684994618/Projects/Link%20in%20Bio/linkedin_zlqzlt.png",
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/akhi_akxu",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684994618/Projects/Link%20in%20Bio/instagram_fljx1e.png",
+    },
+    {
+      name: "GitHub",
+      link: "https://github.com/Akhil-Mahesh",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684995456/Projects/Link%20in%20Bio/1684995382314_lsvmju.png",
+    },
+    {
+      name: "Dev",
+      link: "https://dev.to/akhil-mahesh",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684995483/Projects/Link%20in%20Bio/20230525_114258_xvzell.png",
+    },
+    {
+      name: "Blogger",
+      link: "https://akhil-mahesh.blogspot.com",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684995456/Projects/Link%20in%20Bio/1684995363819_ep2t3u.png",
+    },
+    {
+      name: "Medium",
+      link: "https://medium.com/@akhilmahesh.tech",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684994618/Projects/Link%20in%20Bio/medium_kgd9ye.png",
+    },
+    {
+      name: "YouTube",
+      link: "https://www.youtube.com/@alonephilic",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684995456/Projects/Link%20in%20Bio/1684995339682_fzjaml.png",
+    },
+    {
+      name: "Email",
+      link: "mailto:akhilmahesh.tech@gmail.com",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684994762/Projects/Link%20in%20Bio/gmail_ql6mb5.png",
+    },
+    {
+      name: "Twitter",
+      link: "https://twitter.com/akhi_akxu",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684994618/Projects/Link%20in%20Bio/twitter_ak6glg.png",
+    },
+    {
+      name: "Portfolio",
+      link: "https://akhilmahesh.netlify.app",
+      image: "https://res.cloudinary.com/dqocmr8iu/image/upload/v1684995456/Projects/Link%20in%20Bio/1684995321028_bzj8yw.png",
+    },
+  ];
